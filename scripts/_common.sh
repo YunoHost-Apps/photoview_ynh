7d553cdf25a607d205d757e3c20c03ca5fb1c53c
Félix Piédallu <felix.piedallu@non.se.com>
2024-03-12 12:17:29 +0100

    cleanup traces of libheif

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -19,28 +19,6 @@ function set_go_vars {
 
     go_shims_path=$goenv_install_dir/shims
     go_path_full="$go_shims_path":"$(sudo -u $app bash -c 'echo $PATH')"
-    # heif_lib_path="$install_dir/local/lib":"$(sudo -u $app bash -c 'echo $LIBRARY_PATH')"
-    # heif_ld_lib_path="$install_dir/local/lib":"$(sudo -u $app bash -c 'echo $LD_LIBRARY_PATH')"
-    # heif_cgo_cflags="-I$install_dir/local/include"
-}
-
-function build_libheif {
-    export GOPATH="$install_dir/build/go"
-    export GOCACHE="$install_dir/build/.cache"
-
-    cmake -S "$install_dir/libheif" -B "$install_dir/libheif/build" -DCMAKE_BUILD_TYPE=RELEASE -GNinja
-    cmake --build "$install_dir/libheif/build"
-
-    pushd "$install_dir/libheif" || ynh_die
-        # mkdir -p "$install_dir/local"
-        # chown -R $app:$app "$install_dir/local"
-        ynh_exec_as "$app" ./autogen.sh 2>&1
-        ynh_exec_as "$app" ./configure --prefix="$install_dir/local" --disable-gdk-pixbuf 2>&1
-        ynh_exec_as "$app" make clean 2>&1
-        ynh_exec_as "$app" make 2>&1
-        ynh_exec_as "$app" make install 2>&1
-        ynh_exec_as "$app" make clean 2>&1
-    popd || ynh_die
 }
 
 function build_api {
@@ -48,11 +26,8 @@ function build_api {
 
     gobuild_env=(
         "PATH=$go_path_full"
-        # "LIBRARY_PATH=$heif_lib_path"
-        # "LD_LIBRARY_PATH=$heif_ld_lib_path"
-        # "CGO_CFLAGS=$heif_cgo_cflags"
         "GOENV_VERSION=$go_version"
-        CGO_ENABLED=1
+        "CGO_ENABLED=1"
     )
 
     pushd "$install_dir/sources/api" || ynh_die
